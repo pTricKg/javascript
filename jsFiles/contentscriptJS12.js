cca8d699fffedfa226635e1ef73e87ff567052e2
pTricKg <ptrickg1976@gmail.com>
2014-05-29 00:03:37 -0600

    Add mod contentscript

---
+++ jsFiles/contentscriptJS12.js
@@ -0,0 +1,35 @@
+<script type="text/javascript">
+
+function injectJs(link) {
+var scr = document.createElement('script');
+scr.type="text/javascript";
+scr.src=link;
+document.getElementsByTagName('head')[0].appendChild(scr)
+//document.body.appendChild(scr);
+}
+
+injectJs(chrome.extension.getURL('injected.js'));
+injected.js;
+
+function main() {
+//function test(){
+     if(total == 318338237039211050000)
+                {
+                        alert("Good job!");
+                                alert(("pass =")+pass);
+                                alert(("year =")+year);
+                        window.location.href = year + ".php";
+                }
+                else
+                {
+                        pass++;
+                        //test();
+						main();
+ 
+                        window.location.href = "http://www.enigmagroup.org/missions/basics/js/12/index.php";
+                }
+}
+//test();
+main();
+
+</script>
