040895055928fb03dc18c1198599c555313ffd04
pTricKg <ptrickg1976@gmail.com>
2014-05-29 00:02:58 -0600

    Add og contentscript

---
+++ jsFiles/contentscript.js
@@ -0,0 +1,16 @@
+function injectJs(link) {
+var scr = document.createElement('script');
+scr.type="text/javascript";
+scr.src=link;
+document.getElementsByTagName('head')[0].appendChild(scr)
+//document.body.appendChild(scr);
+}
+
+injectJs(chrome.extension.getURL('injected.js'));
+injected.js :
+
+function main() {
+     alert('Hello World!');
+}
+
+main();
